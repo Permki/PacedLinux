3be8225f320ce4d10a6dbb33cae57495a9645085
Permki <permki@gmail.com>
2021-07-22 14:02:52 +0200

    Update tcp_cubic_paced.c

--- Paced_module/tcp_cubic_paced.c
+++ Paced_module/tcp_cubic_paced.c
@@ -466,17 +466,11 @@ static void bictcp_pace_offload(const struct sock *sk, struct sk_buff *skb){
 	
 	struct bictcp *ca = inet_csk_ca(sk);
 	struct tcp_sock *tp = tcp_sk(sk);
-//Do something to shared mem
-//skb_shared:
-	//__u8		pace_offload;
-	//__u8		pace_offload_rtt;
-	//__u8		pace_offload_cwnd_size;
+
 	skb_shinfo(skb)->pace_offload = 1;
-	skb_shinfo(skb)->pace_offload_rtt = ca->delay_min;
-	skb_shinfo(skb)->pace_offload_rtt = ca->tcp_cwnd;
-	printk(KERN_INFO "tp-sock. srtt_us: %d, snd_cwnd: %d",tp->srtt_us, tp->snd_cwnd);
-	printk(KERN_INFO "modified sk_shinfo. Values are:\nPace_offload: %d\nPace_rtt: %d\nPace_cwnd: %d\n", skb_shinfo(skb)->pace_offload, skb_shinfo(skb)->pace_offload_rtt, skb_shinfo(skb)->pace_offload_cwnd_size);
-	printk(KERN_INFO "modified direct. Values are:\nPace_offload: %d\nPace_rtt: %d\nPace_cwnd: %d\n", 1, ca->curr_rtt, ca->last_cwnd);
+	skb_shinfo(skb)->pace_offload_rtt = tp->srtt_us;
+	skb_shinfo(skb)->pace_offload_cwnd_size = tp->snd_cwnd;
+	printk(KERN_INFO "rtt_us: %d, snd_cwnd: %d",tp->srtt_us, tp->snd_cwnd);
 }
 
 static struct tcp_congestion_ops cubictcp __read_mostly = {
